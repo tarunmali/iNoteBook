a5df47c476caa78c6c379072ffa39bbcace56d2a
Tusharv0607 <2020110785@diu.iiitvadodara.ac.in>
2022-06-25 02:15:52 +0530

    commiting

---
+++ index.js
@@ -0,0 +1,16 @@
+const connect = require('./db');
+const express = require('express');
+const cors = require('cors');
+connect(); 
+
+const app = express();
+const port = 80;
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
+
+app.listen(port, () => {
+  console.log(`iNotebook listening on port ${port}`)
+})
