1361ddf6cbf30740f13d89052c0b94dc74686460
Tushar Vyas <202011075@diu.iiitvadodara.ac.in>
2022-06-25 10:59:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ app.use(cors());
 app.use(express.json());
 app.get('/', function (req, res) {
  res.send("Hello World")
-})
+});
+
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
 app.listen(process.env.PORT || 80, () => {
   console.log(`iNotebook listening on port)
-})
+});
