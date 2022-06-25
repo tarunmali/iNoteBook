9987de2fddfb2d8bdc971d94a68e9229cf37ca6d
Tusharv0607 <2020110785@diu.iiitvadodara.ac.in>
2022-06-25 10:31:45 +0530

    update

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const connect = require('./db');
 const express = require('express');
 const cors = require('cors');
+const env = require("dotenv");
 connect(); 
+env.config();
 
 const app = express();
 const port = 80;
 
 app.use(cors());
 app.use(express.json());
+app.get('/', function (req, res) {
+ res.send("Hello World")
+})
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`iNotebook listening on port ${port}`)
 })
